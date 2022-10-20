ea3a0ed326ce8a38dcb30396d73e99f21c8a4d89
Silverlan <silverlann@gmail.com>
2022-10-20 11:38:36 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <dlfcn.h>
 
-void test()
+int main(int argc,char *argv[])
 {
-  
+  auto hModule = dlopen("libpr_chromium_wrapper.so",RTLD_LAZY | RTLD_GLOBAL);
+  std::cout<<((hModule != NULL) ? "success" : "failure")<<std::endl;
 }
